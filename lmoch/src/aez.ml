24bd487856de46b922896b2a9d89d05ebeaaf307
sr247 <stephane.romany@u-psud.fr>
2018-01-04 17:33:40 +0100

    Ajout Aez et Ast_to_aez

--- lmoch/src/aez.ml
+++ lmoch/src/aez.ml
@@ -9,18 +9,18 @@ type combinator = Cmb_and |Cmb_or | Cmb_impl | Cmb_not
 
 type ident = Ident.t
 
-
 type term =
   | T_cst of constant
   | T_op of term_operator * term * term
-  | T_ite of formula * term * term
-  | T_formula of formul
-
-type formul=
-  || F_term of term
+  | T_ite of formul * term * term
+  | T_formul of formul
+  | T_app ident * int
+    
+type formul =
+  | F_term of term
   | F_cmp of comparison * term * term
   | F_lco of logic_connector * (formula list)
-
+  | F_app of term list
 
 type stream_body = SB_term of term | SB_formula of formula
 
--- lmoch/src/ast_to_aez.ml
+++ lmoch/src/ast_to_aez.ml
@@ -4,6 +4,19 @@ open Aez
 
 
 
+let input_to_aez in =
+  match in with
+    |
+
+
+
+let ast_to_astaez texpr =
+  let input =
+    List.map input_to_aez texpr.tn_input 
+  in
+  let output =
+    List.map in
+  
 
 
 
@@ -11,9 +24,6 @@ open Aez
 
 
 
-
-
-
-
-let to_aez node =
+let to_aez ast_node =
+  let faez = List.map ast_to_astaez ast_node;
   failwith "TODO" 
