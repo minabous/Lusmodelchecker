7d3c4436d0adfecee73590aa59040e8f877be8b8
sr247 <stephane.romany@u-psud.fr>
2018-01-10 01:18:39 +0100

    Avancé sur la fonction ast_to_aez

--- lmoch/src/ast_to_aez.ml
+++ lmoch/src/ast_to_aez.ml
@@ -4,9 +4,9 @@ open Aez
 
 
 
-let input_to_aez in =
-  match in with
-    |
+let input_to_aez input =
+  match input with
+    | _ -> failwith "ast_to_aez::input_to_aez::Not Implemented"
 
 
 
@@ -23,16 +23,15 @@ let ast_to_astaez texpr =
     List.map equs_to_aez texpr.equs in
   let loc =
     List.map loc_to_aez texpr.tn_loc in
-  { name = 
-    
+  { node_name = name;
+    node_input = input;
+    node_output = output;
+    node_vlocal = local;
+    node_equs = equs;
+    node_loc = loc;
   }
-
-
-
-
-
 
 
 let to_aez ast_node =
   let faez = List.map ast_to_astaez ast_node;
-  failwith "TODO" 
+  failwith "to_aez::Under Implementation" 
