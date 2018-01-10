64c84cde4488a34d85000a503a0534503d4e1468
sr247 <stephane.romany@u-psud.fr>
2018-01-10 01:08:31 +0100

    Avancé sur la fonction ast_to_aez

--- lmoch/src/ast_to_aez.ml
+++ lmoch/src/ast_to_aez.ml
@@ -11,12 +11,21 @@ let input_to_aez in =
 
 
 let ast_to_astaez texpr =
+  let name =
+    tnode.name in
   let input =
-    List.map input_to_aez texpr.tn_input 
-  in
+    List.map input_to_aez texpr.tn_input in
   let output =
-    List.map in
-  
+    List.map output_to_aez texpr.tn_output in
+  let local =
+    List.map local_to_aez texpr.tn_local in
+  let equs =
+    List.map equs_to_aez texpr.equs in
+  let loc =
+    List.map loc_to_aez texpr.tn_loc in
+  { name = 
+    
+  }
 
 
 
